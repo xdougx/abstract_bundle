4072ba078705af2948e4c8adbec81207bbe92bca
Douglas Rossignolli <douglas@kanamobi.com.br>
2018-01-24 16:45:28 -0200

    fix require method

--- lib/abstract_bundle.rb
+++ lib/abstract_bundle.rb
@@ -6,7 +6,7 @@ require "abstract_bundle/version"
 require "abstract_bundle/interface"
 require "abstract_bundle/factory"
 
-Dir.glob["concerns/**.rb"].each { |f| require f}
+Dir["concerns/**.rb"].each { |f| require f}
 
 module AbstractBundle
   # standard error for methods not implementeds
