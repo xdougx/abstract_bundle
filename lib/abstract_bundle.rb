17e3038c1882e080de42c216ec4c65b9cadcbc5e
Douglas Rossignolli <douglas@kanamobi.com.br>
2018-02-06 12:30:18 -0200

    fix serialization

--- lib/abstract_bundle.rb
+++ lib/abstract_bundle.rb
@@ -2,10 +2,11 @@ require "active_support"
 require "active_support/inflector"
 require "active_support/concern"
 
+require "serializeble_resource"
+
 require "abstract_bundle/version"
 require "abstract_bundle/interface"
 require "abstract_bundle/factory"
-require "abstract_bundle/serializeble_resource"
 
 require "concerns"
 
--- lib/abstract_bundle/version.rb
+++ lib/abstract_bundle/version.rb
@@ -1,3 +1,3 @@
 module AbstractBundle
-  VERSION = "0.1.11"
+  VERSION = "0.1.11p01"
 end
