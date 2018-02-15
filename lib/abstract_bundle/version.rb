cce3426ee107b258786c824cf4566db01f47deb7
Douglas Rossignolli <douglas@kanamobi.com.br>
2018-02-15 12:41:57 -0200

    version 0.1.15.1 hotfix

--- lib/abstract_bundle/version.rb
+++ lib/abstract_bundle/version.rb
@@ -1,3 +1,3 @@
 module AbstractBundle
-  VERSION = "0.1.15"
+  VERSION = "0.1.15.1"
 end
--- lib/cache/configuration.rb
+++ lib/cache/configuration.rb
@@ -1,15 +1,17 @@
 module Cache
   # Configures global settings for ModelApi
-  #   ModelApi.configure do |config|
+  #   Cache.configure do |config|
   #     config.schema = {
   #       host: "127.0.0.1",
   #       port: 6379,
   #       db: 0
   #     }
+  #     config.ttl = 99999
+  #     config.key = 'user_id'
   #     config.env = :development
   #   end
   def self.configure(&_block)
-    yield(config)
+    yield(config())
   end
 
   # Global settings for ModelApi
@@ -21,7 +23,7 @@ module Cache
   # the api url and authorizations params
   class Configuration
     # principal attributes to execute an http request
-    attr_accessor :schema, :key, :ttl, :envx
+    attr_accessor :schema, :key, :ttl
 
     # constructor that set default values
     def initialize
@@ -31,10 +33,3 @@ module Cache
     end
   end
 end
-
-
-a@redis_key = params[:redis_key] ||= self.ancestors.map(&:name).include?("ActiveRecord::Base") ? "id" : nil
-
-@redis_prefix = params[:redis_prefix] ||= name.underscore
-
-@ttl = params[:ttl] ||= 300
